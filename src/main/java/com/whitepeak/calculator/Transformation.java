ed23e9c3d9398f5bbf1a6a31269550bb7eb23362
Unknown <s.galli11@studenti.uninsubria.it>
2017-02-07 00:41:12 +0100

    Parenthesis Control
    Added additional parenthesis control

--- src/main/java/com/whitepeak/calculator/Transformation.java
+++ src/main/java/com/whitepeak/calculator/Transformation.java
@@ -22,55 +22,56 @@ public class Transformation {
     public String toPostfix() {
         //String formString = this.formatInput();
         //System.out.println("FORMATTATA: "+formString+'\n');
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            switch (ch) {
-                case '+': 
-                case '-':
-                    operator(ch, 1); 
-                    break;
-                    
-                case '*': 
-                case '/':
-                case '^':
-                    operator(ch, 2); 
-                    break; 
-                
-                case '(': 
-                    stack.push(ch);
-                    negOrPos = true;
-                    if(!lastChar.equals("") && !lastChar.equals(" ")){
-                        output = output + " " ;
-                        lastChar = " ";
-                    }
-                    break;
-                
-                case ')': 
-                    parentheses(); 
-                    break;
-                
-                default: 
-                    if((lastChar.equals("+")||lastChar.equals("-")||
-                       lastChar.equals("*")||lastChar.equals("/")||
-                       lastChar.equals("^"))&& negOrPos==false)
-                        output = output + " " + ch;
-                    else
-                        output = output + ch;
-                    lastChar = String.valueOf(ch);
-                    negOrPos = false;
-                    break;
+        if(parenthesesControl()) {
+            for (int i = 0; i < input.length(); i++) {
+                char ch = input.charAt(i);
+                switch (ch) {
+                    case '+': 
+                    case '-':
+                        operator(ch, 1); 
+                        break;
+
+                    case '*': 
+                    case '/':
+                    case '^':
+                        operator(ch, 2); 
+                        break; 
+
+                    case '(': 
+                        stack.push(ch);
+                        negOrPos = true;
+                        if(!lastChar.equals("") && !lastChar.equals(" ")){
+                            output = output + " " ;
+                            lastChar = " ";
+                        }
+                        break;
+
+                    case ')': 
+                        parentheses(); 
+                        break;
+
+                    default: 
+                        if((lastChar.equals("+")||lastChar.equals("-")||
+                           lastChar.equals("*")||lastChar.equals("/")||
+                           lastChar.equals("^"))&& negOrPos==false)
+                            output = output + " " + ch;
+                        else
+                            output = output + ch;
+                        lastChar = String.valueOf(ch);
+                        negOrPos = false;
+                        break;
+                }
+            }
+            while (!stack.isEmpty()) {
+                char ch = stack.pop();
+                if(lastChar.equals(" "))
+                    output = output + ch;    
+                else
+                    output = output + " " + ch;
+                lastChar = String.valueOf(ch); 
+
             }
         }
-        while (!stack.isEmpty()) {
-            char ch = stack.pop();
-            if(lastChar.equals(" "))
-                output = output + ch;    
-            else
-                output = output + " " + ch;
-            lastChar = String.valueOf(ch); 
-                
-        }
-        System.out.println(output);
         return output; 
     }
     
@@ -140,102 +141,24 @@ public class Transformation {
             }
         }
     }
-    //evito di avere i - prima delle parentesi in fase di trasformazione
-    public String formatInput(){
-        StringBuilder formatted = new StringBuilder(input);
-        String minus = "-";
-        String plus= "+";
-        boolean needFormat = true;
-        int length = formatted.length();
-        //itero tutta la stringa partendo dal secondo carattere
-        for (int i=1; i<length; i++){
-            String current = String.valueOf(formatted.charAt(i));
-            String prev = String.valueOf(formatted.charAt(i-1));
-            //valuto il caso in cui la stringa necessiterebbe di essere formattata
-            if(current.equals("(") && prev.equals("-")){
-                //inverto il segno prima della parentesi
-                formatted.setCharAt(i-1, plus.charAt(0));
-                int nested = 0;
-                //itero l stringa a partire dal carattere dopo la tonda per cambiare ogni segno in parentesi
-                //tralasciando le innestate, finchè la prima non viene chiusa
-                int x = i + 1;
-                while(nested != -1){
-                    char ch = formatted.charAt(x);
-                    switch(ch){
-                        case '+':
-                            if (nested == 0)
-                                formatted.setCharAt(x, minus.charAt(0));
-                            x++;
-                            break;
-                            
-                        case '-':
-                            if (nested == 0)
-                                formatted.setCharAt(x, plus.charAt(0));
-                            x++;
-                            break;
-                            
-                        case '(':
-                            nested = nested+1;
-                            x++;
-                            break;
-                            
-                        case ')':
-                            nested = nested-1;
-                            x++;
-                            break;
-                         
-                        default:    //Se è un numero positivo senza segno ovvero numero intero
-                                    //preceduto da ( o * o / o ^ devo trasformarlo in negativo e
-                                    //metterlo in parentesi se preceduto da * o / o ^
-                            try {
-                                Integer.parseInt(String.valueOf(ch));
-                                if(nested == 0){
-                                    switch(formatted.charAt(x-1)){
-                                        case '(':
-                                            formatted.insert(x, "-");
-                                            length++;
-                                            x++;
-                                            System.out.println("Inserito il meno " + '\n');
-                                            break;
-                                            
-                                        case '*':
-                                        case '/':
-                                        case '^':
-                                            formatted.insert(x, "-");
-                                            formatted.insert(x, "(");
-                                            length = length+2;
-                                            x = x+2;
-                                            //scorro per stabilire dove chiudere la parentesi, in base a quante cifre ha il n
-                                            boolean close = false;
-                                            int n = x + 1;
-                                            while (close == false){
-                                                try{
-                                                    Integer.parseInt(String.valueOf(formatted.charAt(n)));
-                                                    n++;
-                                                 
-                                                } catch (NumberFormatException e) {
-                                                    //Se non trovo un numero chiudo la parentesi
-                                                    formatted.insert(n, ")");
-                                                    length++;
-                                                    x++;
-                                                    close = true;
-                                                }
-                                                
-                                            }
-                                            System.out.println("Inserito il meno " + '\n');
-                                            break;
-                                            
-                                    }
-                                }
-                            } catch (NumberFormatException e) {
-                                //non fare niente se trova . * / ^
-                            }
-                            x++; 
-                            break;
-                    }
-                }
+    
+    public boolean parenthesesControl () {
+        int openPars = 0;
+        int closedPars = 0;
+        for(int x=0; x<input.length(); x++){
+            switch(input.charAt(x)){
+                case '(':
+                    openPars++;
+                    if(input.length()>0 &&(input.charAt(x-1) == '.' || Character.isDigit(input.charAt(x-1)) ))
+                        return false;
+                    break;
+                case ')':
+                    closedPars++;
+                    if((x+1<=input.length())&&(input.charAt(x+1) == '.' || Character.isDigit(input.charAt(x+1)) ))
+                        return false;
+                    break;
             }
         }
-        return String.valueOf(formatted);
+        return openPars != closedPars;
     }
 }
